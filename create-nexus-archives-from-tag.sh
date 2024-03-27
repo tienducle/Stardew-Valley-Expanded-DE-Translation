d9a56f931ba0ef9c532da8c9df9f69303d88f54a
Tien Duc Le <6162574+tienducle@users.noreply.github.com>
2024-03-27 19:21:51 +0100

    Adjusted create-nexus-archives-from-tag.sh script to include installer

--- create-nexus-archives-from-tag.sh
+++ create-nexus-archives-from-tag.sh
@@ -12,8 +12,10 @@ git checkout $TAG
 
 find . -name ".DS_Store" -exec rm {} \;
 rm -rf tmp/*.zip
+cp "nsis/Stardew Valley Expanded - German Translation.exe" .
 zip -r "tmp/Grandpa's Farm.zip" "Grandpa's Farm"
 zip -r "tmp/Immersive Farm 2 Remastered.zip" "Immersive Farm 2 Remastered"
-zip -r "tmp/Stardew Valley Expanded.zip" "Stardew Valley Expanded"
+zip -r "tmp/Stardew Valley Expanded.zip" "Stardew Valley Expanded" "Stardew Valley Expanded - German Translation" "Stardew Valley Expanded - German Translation.exe" -x "*default.json"
+rm -rf "./Stardew Valley Expanded - German Translation.exe"
 
 git checkout main
